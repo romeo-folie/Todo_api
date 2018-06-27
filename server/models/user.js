df39962df6d2f7dbf3da817786064092fb5fbddf
Romeo Folie <romeofolie1@gmail.com>
2018-06-27 09:04:09 +0000

    password hashing before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-
+const bcrypt = require('bcryptjs');
 
 //user model
 var UserSchema = new mongoose.Schema({
@@ -74,6 +74,23 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function(next){
+  var user = this;
+  if(user.isModified('password')){
+    //user.password
+    bcrypt.genSalt(10, (err, salt)=>{
+      bcrypt.hash(user.password, salt, (err, hash)=>{
+        user.password = hash;
+        next();
+      });
+    });
+  }else{
+    next();
+  }
+
+
+});
 
 var User = mongoose.model('User',UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -113,8 +113,6 @@ app.post('/users', (req, res)=>{
   });
 });
 
-//middleware
-
 app.get('/users/me', authenticate, (req, res)=>{
   res.send(req.user);
 });
