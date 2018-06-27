c435496ff5fbca72ac134427e8fc1fa1e5b13fae
Romeo Folie <romeofolie1@gmail.com>
2018-06-27 16:18:43 +0000

    added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 const {authenticate} = require('./middleware/authenticate');
+const bcrypt = require('bcryptjs');
 
 var app = express();
 
@@ -112,6 +113,21 @@ app.post('/users', (req, res)=>{
     res.status(400).send(e);
   });
 });
+
+
+// POST /users/login
+app.post('/users/login', (req, res)=>{
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) =>{
+    user.generateAuthToken().then((token) =>{
+      res.header('x-auth',token).status(200).send(user)
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
 
 app.get('/users/me', authenticate, (req, res)=>{
   res.send(req.user);
